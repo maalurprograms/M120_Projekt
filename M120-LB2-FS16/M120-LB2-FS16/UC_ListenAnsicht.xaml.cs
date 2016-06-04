c0227bf9165859e207c8fb17b03b6dbe3a3cf2bb
maalurprograms <jonascosandey@hotmail.com>
2016-06-04 17:38:37 +0200

    uc_list

---
+++ M120-LB2-FS16/M120-LB2-FS16/UC_ListenAnsicht.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M120_LB2_FS16
+{
+    /// <summary>
+    /// Interaktionslogik für UC_ListenAnsicht.xaml
+    /// </summary>
+    public partial class UC_ListenAnsicht : UserControl
+    {
+        public UC_ListenAnsicht()
+        {
+            InitializeComponent();
+            foreach (Einsatz einsatz in Bibliothek.Einsatz_Alle())
+            {
+                listen_ansicht.
+            }
+        }
+    }
+}
